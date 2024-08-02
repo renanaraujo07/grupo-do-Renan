fd772e4a6cffcafbf0720dab900e877a40b5f6d3
leeticia007 <leticiadinacampos@gmail.com>
2024-08-02 10:01:02 -0300

    Meu teste

---
+++ teste.py
@@ -0,0 +1,14 @@
+#Isso serve para mostrar olá mundo
+
+"""
+Este é um comentário
+nao sei o que colocar
+kkk
+"""
+
+Print ("Seja bem vindo")
+nome = input 
+("Digite seu nome")
+
+print ("Bem vindo,", nome + "!")
+
