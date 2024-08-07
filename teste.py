8bc15c78b4fcdf3ba58f97845a9a4e2e9a2cde9c
leeticia007 <leticiadinacampos@gmail.com>
2024-08-07 09:58:43 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,14 +1,24 @@
 #Isso serve para mostrar olá mundo
 
 """
-Este é um comentário
+Este é um comentario
 nao sei o que colocar
 kkk
 """
 
-Print ("Seja bem vindo")
-nome = input 
-("Digite seu nome")
+print ("Seja bem vindo")
+nome = input ("Digite seu nome")
 
-print ("Bem vindo,", nome + "!")
+print("Bem vindo,", nome + "!")
 
+print("Em que posso te ajudar?")
+
+print("1-consultar mensalidade")
+print("2-consultar diária")
+print("3-consultar promoções")
+print("4-cancelar vínculo")
+
+opcao = int(input("Digite o numero da opção desejada:"))
+
+if opcao == 1: 
+print("")
