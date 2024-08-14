9809435b37e944ec8b17458a629ff8930865d631
Renan Brito de Araujo <160871561+renanaraujo07@users.noreply.github.com>
2024-08-14 09:55:42 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,5 @@
+import time
+
 #Isso serve para mostrar olá mundo
 
 """
@@ -5,20 +7,36 @@ Este é um comentario
 nao sei o que colocar
 kkk
 """
-
+tempo = 2;
 print ("Seja bem vindo")
-nome = input ("Digite seu nome")
+nome = input ("Digite seu nome:")
 
 print("Bem vindo,", nome + "!")
 
+time.sleep(tempo)
+
 print("Em que posso te ajudar?")
 
-print("1-consultar mensalidade")
-print("2-consultar diária")
-print("3-consultar promoções")
-print("4-cancelar vínculo")
-
+print("1-Consultar mensalidade")
+print("2-Consultar diária")
+print("3-Consultar promoções")
+print("4-Cancelar vínculo")
 opcao = int(input("Digite o numero da opção desejada:"))
 
+time.sleep(tempo)
+
 if opcao == 1: 
-print("")
+    print("R$ 200,00")
+
+elif opcao == 2:
+    print("R$ 30,00 ")
+
+elif opcao == 3:
+    print("Promo de inauguração, 10 meses por R$1000,00 ")
+
+elif opcao == 4:
+    print("vinculo cancelado, sob multa de R$ 50,00 ")
+
+else:
+    print("erro")
+
